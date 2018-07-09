9788934bbbaad7f2ccdecff8429239394c2944cd
fuele <37090676+fuele@users.noreply.github.com>
2018-07-09 00:21:56 +0000

    File_recorder now purges records from the list that are older than 15min

--- wifi-monitor/file_recorder.py
+++ wifi-monitor/file_recorder.py
@@ -1,6 +1,7 @@
 import packet
 import logging
 import os
+import datetime
 
 class File_Recorder:
     """
@@ -9,20 +10,22 @@ class File_Recorder:
 
     def __init__ (self):
         class_name=os.path.basename(__name__)
-        #self.client_file = open('clients.txt','w')
         self.logger = logging.getLogger('wifi-monitor' + '.' + class_name)
-        self.clients = dict() 
+        self.clients = dict()
+        self.client_ttl_sec = 900 
     #end constructor
 
 
 
     def add_client(self, client_packet):
-        
+
         if(client_packet.src_mac not in self.clients):
             self.logger.debug("Found unique client " + client_packet.src_mac + ".")
             self.clients[client_packet.src_mac]= client_packet
             self.update_client_file()
         #end if
+
+        self.purge_old_clients()
 
     #end function
 
@@ -45,7 +48,28 @@ class File_Recorder:
         Goes through the client list and removes any clients that
         haven't been seen for a while
         """
-        pass
+
+        #we cannot modify a dict while iterating through it
+        clients_to_remove = set()
+
+        for key in self.clients:
+            age_in_sec = (datetime.datetime.now() - self.clients[key].timestamp).total_seconds()
+
+            if age_in_sec > self.client_ttl_sec:
+                self.logger.debug("Staging old client " + key + " for deletion")
+                clients_to_remove.add(key)
+            #end if
+        #end for
+        
+        if len(clients_to_remove) > 0:
+            for key in clients_to_remove:
+                self.logger.debug("Attempting to delete " + key)
+                del self.clients[key]
+            #end for
+            self.update_client_file()
+        #end if
+
+        
     #end function
 
 #end class
