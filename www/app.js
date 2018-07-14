eae5a79eafdbb9bf46fe95cfb198245cb756ac4e
fuele <37090676+fuele@users.noreply.github.com>
2018-07-14 16:03:06 +0000

    Cleaned up app.js file a bit

--- www/app.js
+++ www/app.js
@@ -1,15 +1,16 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+var express = require('express')
+var app = express()
+var path = require('path')
+var port = 3000
 
 //app.use(express.static('/home/donovan/code/doorman/www/public'));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')))
 
 //app.get('/', function (req, res) {
 //    res.send('Hello World!');
 //});
 
-app.listen(80, function() {
-    console.log('Example app listening on port 80.');
-    console.log(path.join(__dirname,'public'));
+app.listen(port, () => {
+    console.log('Example app listening on port ' + port)
+    console.log(path.join(__dirname,'public'))
 });
