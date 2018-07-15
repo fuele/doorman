06f902dc005e857a333668778911d4d4a204b8fa
fuele <37090676+fuele@users.noreply.github.com>
2018-07-15 00:57:33 +0000

    Added an API that reads from a list of clients in a local mongo database

--- www/app.js
+++ www/app.js
@@ -7,12 +7,28 @@ var MongoClient = require('mongodb').MongoClient
 var dburi = 'mongodb://localhost:27017'
 var dbName = 'doorman'
 var currentClientsCollection = 'currentClients'
+var nicknameCollection = 'macNicknames'
 
 var db
 
 //app.use(express.static(path.join(__dirname,'public')))
 
+function getNick(mac){
+    console.log('Looking for nickname for ' +mac)
+    db.collection(nicknameCollection).find({'mac':mac},{_id:0}).toArray( (err,results) => {
+        if(err){
+            console.log('Failed to find mac in nickname collection')
+	    return {'mac':mac, 'nick':'unknown device'}
+	}else{
+            console.log('Found nickname')
+	    console.log('results: ' + results)
+	    return(results)
+	}//end if
+    })//end function
+}//end function
 
+
+//list all the clients currently connected to the wifi
 app.get('/api/clients', (req, res) => {
     db.collection(currentClientsCollection).find({},{_id:0}).toArray( (err, results) => {
         console.log(results)
